3c2358284e4344cdba1cfb74354277b8ff17714e
PapLion <ZhoyElPutoAmoXD@gmail.com>
2023-08-15 19:23:17 -0500

    Cambio a ruby

---
+++ Important/Ruby/Curso-Online/#3Variables.rb
@@ -0,0 +1,34 @@
+#Variables en rust
+variable = "Uriel"
+puts Variable
+variable = 20;
+puts Variable
+numero_entero = 42;
+puts numero_entero
+numero_flotante = 3.14159
+puts numero_flotante
+String (Cadena): Representa texto. Puede definirse usando comillas simples o dobles. Ejemplo: 'Hola, mundo', "Ruby es genial".
+
+Symbol (Símbolo): Representa nombres inmutables utilizados como identificadores. Ejemplo: :nombre, :edad.
+
+Boolean (Booleano): Representa valores de verdad, true o false.
+
+Array (Arreglo): Representa una colección ordenada de elementos. Ejemplo: [1, 2, 3], ['rojo', 'verde', 'azul'].
+
+Hash (Hash o Diccionario): Representa una colección de pares clave-valor. Ejemplo: { 'nombre' => 'Juan', 'edad' => 30 }.
+
+NilClass (Clase Nil): Representa la ausencia de un valor. Se utiliza principalmente para indicar la falta de valor en una variable.
+
+Range (Rango): Representa un conjunto de valores secuenciales. Ejemplo: 1..10 (rango inclusivo), 1...10 (rango excluyente).
+
+Regexp (Expresión Regular): Representa patrones de búsqueda en cadenas.
+
+Proc y Lambda: Son objetos que encapsulan bloques de código, permitiendo ser pasados como argumentos y ejecutados posteriormente.
+
+Class (Clase): Representa las clases en Ruby, que son los moldes para la creación de objetos.
+
+Module (Módulo): Representa módulos en Ruby, que permiten la agrupación y el espacio de nombres para métodos y constantes.
+
+File (Archivo): Representa archivos en el sistema de archivos.
+
+IO (Entrada/Salida): Representa las entradas y salidas de datos, como la lectura y escritura en archivos y la entrada/salida estándar.
---
+++ Md/Important/RUby.md
@@ -0,0 +1,28 @@
+# Ruby
+## Caracteristicas
+    -Es un lenguaje interpretado (sin punto y coma(Pero los puedes usar de todas formas))
+     ni tampoco existe un compilador si no que se ejecuta linea a linea.
+    -Es de tipado dinamico (No es tan forzado el tipo osea declaras que es x tipo
+     y esa variable solo puede usar ese tipo).
+    -Todo en ruby es un objeto.
+    -Productividad (Se puede hacer proyecto mas rapidos que en otro lenguaje. Como herramientas
+     que pueden ahorrarte tiempo).
+    -Puedes casi casi reescribir el lenguaje agregando tus propios metodos.
+    -Inspirado en Lisp, Perl, Smalltalk, Eiffel y Ada.
+    -Busca ser natural y no simple.
+    -Prefiere el ingles, que la puntuacion.
+    -Existes multiples implementaciones del lenguaje (como en c o java).
+    -Ha inspirado la creacion de otros lenguajes.
+    -Principal uso en la web.
+    -Se lo utiliza tambien para hacer utilidades de la terminal.
+    -Mobile nativo con RubyMotion.
+    -Crear interfaces graficas o programas de escritorio con Qt o con swing, usando jRuby.
+    -Quienes usan ruby?.
+## Porque aprender rubby?
+    -Mejorar productividad.
+    -Te importa la expresividad.
+    -Si quieres un lenguaje flexible.
+    -Si no te gusta repetir código.
+    -Si quieres ser parte de una comunidad activa.
+## Variables
+    Solamente escrite el nombre de la variable y que le quieres poner (IMPORTANTE USAR SNAKE_CASE)
