55b772eab41336f22aacab3eff38d650a4d4d46b
PapEstudios <137430012+PapEstudios@users.noreply.github.com>
2023-08-30 09:44:33 -0500

    Agregando el sistema de birfurcacion

---
+++ Secundary/C-C++/Bifurcacion.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int edad;
+    printf("Ingrese la edad: ");
+    scanf("%d", &edad);
+    if (edad >= 18 && edad <=35)
+    {
+        printf("uted tiene %d por lo tanto es aceptado", edad);
+    }
+    else
+    {
+        printf("uted tiene %d por lo tanto no es aceptado", edad);
+    }
+    getch();
+}
