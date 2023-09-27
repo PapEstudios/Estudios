d752033a8e850b0994930baa8197f31ad0dab359
PapEstudios <137430012+PapEstudios@users.noreply.github.com>
2023-09-27 09:52:40 -0500

    forpepito

---
+++ Secundary/C-C++/forcongotoxy.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    int pe, pa;
+   printf("Ingrese el numero de la tabla que desea: ");
+   scanf("%d",&pe);
+   printf("Ingrese el numero hasta el que desea llegar: ");
+   scanf("%d",&pa);
+   gotoxy(29,4);printf("Tabla del %d",pe);
+   for(int i = 1; i <= pa; i++){
+
+    int r = pe * i;
+    gotoxy(29,4+i);
+    printf("%d x %d = %d \n", pe, i , r);
+   }
+   getch();
+}
